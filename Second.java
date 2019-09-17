40dca94f69d4f49893ecd7e2f58e22c957d5019e
asifraza92019 <55436780+asifraza92019@users.noreply.github.com>
2019-09-17 15:41:56 +0530

    added new functionalities to test

--- Second.java
+++ Second.java
@@ -3,5 +3,11 @@ class Second
     public static void main(String ... args)
       { 
         System.out.println("Hello from Second Program") ;
+        
+         int a =10;
+        int b =20;
+             int c = a+b;
+        System.out.println("value of sum is =" + c);
+        
         } 
   }
