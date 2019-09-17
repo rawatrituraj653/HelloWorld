c147d312379040df6ead2c118944a9393d70802d
asifraza92019 <asifraza92019@gmail.com>
2019-09-17 15:50:32 +0530

    some functionalities modified

--- Second.java
+++ Second.java
@@ -4,10 +4,15 @@ class Second
       { 
         System.out.println("Hello from Second Program") ;
         
-         int a =10;
-        int b =20;
-             int c = a+b;
+          int a =10;
+          int b =20;
+          int c = a+b;
         System.out.println("value of sum is =" + c);
+		 
+		 String s1 = "hello" ;
+		  String  s2 = "world";
+		  String s3 = s1+s2;
+		  System.out.println(s3);
         
         } 
   }
